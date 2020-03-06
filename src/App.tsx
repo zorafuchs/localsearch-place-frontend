d0ae19de92162fa5105d543d3de01ab4ac8b5c14
Zora Fuchs <>
2020-03-06 10:35:53 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Container, Jumbotron } from 'react-bootstrap';
+
+export const App: React.FC = () => (
+  <Container>
+    <Jumbotron>
+      <h1>Hello World</h1>
+    </Jumbotron>
+  </Container>
+);
+
+export default App;
